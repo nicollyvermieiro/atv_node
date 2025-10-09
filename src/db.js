3b5ba41b6241cc55bf522233f6b3e8c0c2f75984
Laboratorio <laboratorio@lab.unigran>
2025-10-09 20:40:52 -0300

    Docker cd

--- src/db.js
+++ src/db.js
@@ -1,21 +1,12 @@
-const { Pool } = require('pg');
-require('dotenv').config();
+// db.js
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: process.env.DB_USER || "postgres",
-  host: process.env.DB_HOST || "db", // importante: serviço do docker-compose
-  database: process.env.DB_NAME || "clientesdb",
-  password: process.env.DB_PASSWORD || "postgres",
-  port: process.env.DB_PORT || 5432,
-});
-
-pool.on('connect', () => {
-  console.log('✅ Conectado ao PostgreSQL com sucesso!');
-});
-
-pool.on('error', (err) => {
-  console.error('❌ Erro inesperado no cliente do PostgreSQL', err);
-  process.exit(-1);
+  host: process.env.PGHOST || "127.0.0.1", // hostname do container
+  port: process.env.PGPORT || 5432,        // porta do Postgres
+  user: process.env.PGUSER || "postgres",  // usuário
+  password: process.env.PGPASSWORD || "postgres", // senha
+  database: process.env.PGDATABASE || "clientesdb", // nome do banco
 });
 
 module.exports = pool;
